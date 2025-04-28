03ef933487462dd3a07156498310ebf29371957c
Cuongrevival <trainingsession495@gmail.com>
2025-04-28 16:37:35 +0700

    Delete Lesson9 directory

--- Lesson9/customer-manage-aspect/src/main/java/org/example/repository/ICustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICustomerRepository extends JpaRepository<Customer, Long> {
-}
